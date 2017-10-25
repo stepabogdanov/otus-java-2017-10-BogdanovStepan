462ddf1f05cf3cb89b061f4567ae0d73ec9ee200
Stepan Bogdanov <stepanbogdanov>
2017-10-25 12:45:52 +0500

    complete task

--- HW02/src/main/java/hw02size/Main.java
+++ HW02/src/main/java/hw02size/Main.java
@@ -1,7 +1,8 @@
 package hw02size;
 
-import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
+import javax.lang.model.element.TypeElement;
+import javax.lang.model.element.TypeParameterElement;
+import java.lang.reflect.*;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -9,27 +10,28 @@ import java.util.Map;
 
 public class Main {
     public static void main(String[] args) throws InstantiationException,
-                                            IllegalAccessException,
-                                            InterruptedException {
+            IllegalAccessException,
+            InterruptedException, NoSuchMethodException, InvocationTargetException {
         Object object = new Object();
-        String stringPoll = new String();
+        String stringPoll = new String("");
         String string = new String(new char[0]);
-        Integer integer = new Integer(1);
+        Integer integer = new Integer(0);
         MyClass myClass = new MyClass();
         List<Integer> list = new ArrayList<>();
-        List<Integer> list2 = new ArrayList<>(100);
         Map<String, String> map = new HashMap<>();
+        int[] array = new int[0];
 
+        MeasureObjects.measureSizeObject(object);
+        MeasureObjects.measureSizeObject(string);
+        MeasureObjects.measureSizeObject(stringPoll);
+        MeasureObjects.measureSizeObject(integer);
+        MeasureObjects.measureSizeObject(array);
+        MeasureObjects.measureSizeObject(myClass);
+        MeasureObjects.measureSizeObject(list);
+        MeasureObjects.measureConteiner(list,10);
+        MeasureObjects.measureSizeObject(map);
+        MeasureObjects.measureConteiner(map, 20);
 
-        int[] i1 = new int[0];
-
-        MeasureObjects.MeasureSize(object);
-        MeasureObjects.MeasureSize(string);
-        MeasureObjects.MeasureSize(stringPoll);
-        //MeasureObjects.MeasureSize(integer);  // Integer бросает исключеня
-        MeasureObjects.MeasureSize(myClass);
-        MeasureObjects.MeasureSize(list);
-        MeasureObjects.MeasureSize(list2);     // размер одинаковый с list
-        MeasureObjects.MeasureSize(map);
     }
+
 }
--- HW02/src/main/java/hw02size/MeasureObjects.java
+++ HW02/src/main/java/hw02size/MeasureObjects.java
@@ -1,49 +1,159 @@
 package hw02size;
-
-import java.lang.management.ManagementFactory;
-import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 
 public class MeasureObjects {
 
-    static void MeasureSize (Object object) throws IllegalAccessException,
-            InstantiationException, InterruptedException {
-        final int SIZE = 20_000_000;
-        long memBefore = 0, memAfter = 0, sizeOfObject = 0;
-        Runtime runtime = Runtime.getRuntime();
+    private final static int SIZE = 20_000_000;
+    private static Class clazz;
+    private static long memBefore = 0, memAfter = 0, sizeOfObject = 0;
+    private static Runtime runtime;
+
+    public static void measureSizeObject(Object object) throws IllegalAccessException,
+            InstantiationException, InterruptedException, NoSuchMethodException, InvocationTargetException {
+
+        runtime = Runtime.getRuntime();
         boolean loop = true;
-        Class clazz = object.getClass();
+        clazz = object.getClass();
 
         System.out.println("total memory before: " + runtime.totalMemory());
         System.out.println("free memory before:  " + runtime.freeMemory());
+
+        if (clazz.isArray()) {
+            allocateMemoryArray();
+
+        } else if (clazz.equals(String.class)) {
+            allocateMemory(clazz, "");
+
+        } else if (clazz.equals(Integer.class)) {
+            allocateMemory(1);
+        }
+        else {
+            allocateMemory();
+        }
+
+        System.gc();
+        Thread.sleep(1000);
+        sizeOfObject = (memAfter - memBefore) / SIZE;
+        System.out.println("-----------------------------");
+        System.out.println("used memory after:   " + memAfter);
+        System.out.println("free memory after:   " + runtime.freeMemory());
+        System.out.println("total memory after:  " + runtime.totalMemory());
+        System.out.println("Object class name: " + clazz.getName());
+
+        if (sizeOfObject % 8 == 0) {
+            System.out.println("size of object: " + sizeOfObject);
+        } else {
+            sizeOfObject = ((sizeOfObject / 8) * 8) + 8;
+            System.out.println("size of object: " + sizeOfObject);
+        }
+        System.out.println();
+
+    }
+
+    public static void measureConteiner (Object object, int initialCapacity) throws
+                                        InterruptedException, IllegalAccessException,
+                                        InstantiationException, NoSuchMethodException,
+                                        InvocationTargetException {
+        runtime = Runtime.getRuntime();
+        boolean loop = true;
+        clazz = object.getClass();
+
+        System.out.println("total memory before: " + runtime.totalMemory());
+        System.out.println("free memory before:  " + runtime.freeMemory());
+
+        allocateMemory(initialCapacity);
+
+
+        System.gc();
+        Thread.sleep(1000);
+        sizeOfObject = (memAfter - memBefore) / SIZE;
+        System.out.println("-----------------------------");
+        System.out.println("used memory after:   " + memAfter);
+        System.out.println("free memory after:   " + runtime.freeMemory());
+        System.out.println("total memory after:  " + runtime.totalMemory());
+        System.out.println("Object class name: " + clazz.getName() +  " initialCapacity: " + initialCapacity);
+
+        if (sizeOfObject % 8 == 0) {
+            System.out.println("size of object: " + sizeOfObject);
+        } else {
+            sizeOfObject = ((sizeOfObject / 8) * 8) + 8;
+            System.out.println("size of object: " + sizeOfObject);
+        }
+        System.out.println();
+
+    }
+
+    private static void allocateMemory(Class clazz, String instanceParam) throws InterruptedException, NoSuchMethodException,
+            IllegalAccessException, InvocationTargetException,
+            InstantiationException {
+        boolean loop = true;
         while (loop) {
-            Object [] arrayOfObjects = new Object[SIZE];
+            Object[] arrayOfObjects = new Object[SIZE];
             System.gc();
             Thread.sleep(1000);
             memBefore = runtime.totalMemory() - runtime.freeMemory();
             System.out.println("used memory before:   " + memBefore);
             for (int i = 0; i < SIZE; i++) {
-                arrayOfObjects[i]  = clazz.newInstance();  // правильно ли таким образом создавать объект
-                }
+                arrayOfObjects[i] = clazz.getConstructor(clazz).newInstance(instanceParam);
+            }
             memAfter = runtime.totalMemory() - runtime.freeMemory();
             loop = false;
-        }
-        System.gc();
-        Thread.sleep(1000);
-        sizeOfObject = (memAfter - memBefore) /SIZE;
-        System.out.println("-----------------------------");
-        System.out.println("used memory after:   " + memAfter);
-        System.out.println("free memory after:   " + runtime.freeMemory());
-        System.out.println("total memory after:  " + runtime.totalMemory());
 
-        System.out.println("Object class name: " + clazz.getName());
-        if (sizeOfObject%8 == 0){
-            System.out.println("size of object: " + sizeOfObject);
         }
-        else {
-            sizeOfObject = ((sizeOfObject/8) * 8) + 8;
-            System.out.println("size of object: " + sizeOfObject);
+    }
+    private static void allocateMemory(int instanceParam) throws InterruptedException, NoSuchMethodException,
+            IllegalAccessException, InvocationTargetException,
+            InstantiationException {
+        boolean loop = true;
+        while (loop) {
+            Object[] arrayOfObjects = new Object[SIZE];
+
+            System.gc();
+            Thread.sleep(1000);
+            memBefore = runtime.totalMemory() - runtime.freeMemory();
+            System.out.println("used memory before:   " + memBefore);
+
+            for (int i = 0; i < SIZE; i++) {
+                arrayOfObjects[i] = clazz.getConstructor(int.class).newInstance(instanceParam);
+            }
+
+            memAfter = runtime.totalMemory() - runtime.freeMemory();
+            loop = false;
+
         }
-        System.out.println();
+    }
+
+    private static void allocateMemory() throws InterruptedException, IllegalAccessException, InstantiationException {
+        boolean loop = true;
+        while (loop) {
+            Object[] arrayOfObjects = new Object[SIZE];
+            System.gc();
+            Thread.sleep(1000);
+            memBefore = runtime.totalMemory() - runtime.freeMemory();
+            System.out.println("used memory before:   " + memBefore);
+            for (int i = 0; i < SIZE; i++) {
+                arrayOfObjects[i] = clazz.newInstance();             // правильно ли таким образом создавать объект
+            }
+            memAfter = runtime.totalMemory() - runtime.freeMemory();
+            loop = false;
+
+        }
+    }
+
+    private static void allocateMemoryArray() throws InterruptedException{
+        boolean loop = true;
+        while (loop) {
+            Object[] arrayOfObjects = new Object[SIZE];
+            System.gc();
+            Thread.sleep(1000);
+            memBefore = runtime.totalMemory() - runtime.freeMemory();
+            System.out.println("used memory before:   " + memBefore);
+            for (int i = 0; i < SIZE; i++) {
+                arrayOfObjects[i] = new int[0];
+            }
+            memAfter = runtime.totalMemory() - runtime.freeMemory();
+            loop = false;
+
+        }
     }
 }
