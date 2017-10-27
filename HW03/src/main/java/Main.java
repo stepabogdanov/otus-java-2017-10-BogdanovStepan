e35f4c5f65ea9aff31613ca031a2bbcc957eb670
stepabogdanov <stepabogdanov@yandex.ru>
2017-10-27 13:17:00 +0500

    allmost done

--- HW03/src/main/java/Main.java
+++ HW03/src/main/java/Main.java
@@ -1,18 +1,49 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Main {
-
     public static void main(String[] args) {
-        MyArrayList<String> list = new MyArrayList();
+        MyArrayList<String> list = new MyArrayList<>();
+        List<String> listFull = new ArrayList();
+        listFull.add("80");
+        listFull.add("82");
+        listFull.add("83");
+        listFull.add("83");
+        listFull.add("83");
+        listFull.add("83");
+        listFull.add("83");
+        listFull.add("83");
+        listFull.add("83");
+
+
+
+        String[] arr = new String[0];
+
+        System.out.println("size: " + list.size());
+        System.out.println("isEmpty: " +list.isEmpty());
         list.add("1");
-        list.add("2");
-        list.add("4");
         list.add("5");
-        list.add("8");
-        list.enlargeArrayToTen();
-        System.out.println(list.isEmpty());
-        //System.out.println(list.contains("2"));
-        System.out.println(Arrays.toString(list.toArray()));
+        list.add("7");
+        list.add("7");
+        list.add("7");
+        list.add("7");
+
+
+
+        System.out.println("size: " + list.size());
+        System.out.println("isEmpty: " +list.isEmpty());
+        System.out.println("toArray: " + Arrays.toString(list.toArray()));
+        //System.out.println("enlarge: "+ list.enlargeArrayToTen()+ " size after: " + list.size());
+        System.out.println("size: " + list.size());
+        System.out.println("toArray: " + Arrays.toString(list.toArray()));
+        System.out.println("contains 3 ?: " + list.contains("3"));
+        System.out.println("remove 3: " +list.remove("3"));
+        list.add("12");
+
+
+        list.addAll(listFull,5);
+        System.out.println("toArray: " + Arrays.toString(list.toArray()));
 
 
     }
