20bd0415e53df1778ee13712e47df182301cc553
stepabogdanov <stepabogdanov@yandex.ru>
2017-11-07 11:03:48 +0500

    Update Main.java

--- HW03/src/main/java/Main.java
+++ HW03/src/main/java/Main.java
@@ -1,3 +1,9 @@
+import java.util.*;
+
+public  class Main {
+    public static void main(String[] args) {
+
+
         MyArrayList<String> list = new MyArrayList<>();
         List<String> listFull = new ArrayList();
         List<String> listToCopy = new ArrayList(40);
@@ -13,42 +19,44 @@
         String[] arr = new String[0];
 
         System.out.println("size: " + list.size());
-        System.out.println("isEmpty: " +list.isEmpty());
+        System.out.println("isEmpty: " + list.isEmpty());
         list.add("1");
         list.add("5");
         list.add("7");
 
 
-
-
         System.out.println("size: " + list.size());
-        System.out.println("isEmpty: " +list.isEmpty());
+        System.out.println("isEmpty: " + list.isEmpty());
         System.out.println("toArray: " + Arrays.toString(list.toArray()));
         //System.out.println("enlarge: "+ list.enlargeArrayToTen()+ " size after: " + list.size());
         System.out.println("size: " + list.size());
         System.out.println("toArray: " + Arrays.toString(list.toArray()));
         System.out.println("contains 3 ?: " + list.contains("3"));
-        System.out.println("remove 3: " +list.remove("3"));
+        System.out.println("remove 3: " + list.remove("7"));
         list.add("12");
 
 
-        list.addAll(listFull,5);
+        list.addAll(listFull, 5);
         System.out.println("toArray: " + Arrays.toString(list.toArray()));
+
+        Collections.copy(listToCopy, listFull);
+        System.out.println("toArray_listToCopy: " + Arrays.toString(listToCopy.toArray()));
 
         Collections.copy(list, listToCopy);
         System.out.println("toArray: " + Arrays.toString(listToCopy.toArray()));
+        list.add("50");
+        list.add("58");
+        list.add("56");
         Collections.sort(list);
-
         System.out.println("toArray: " + Arrays.toString(list.toArray()));
 
-        ListIterator listIterator = list.listIterator();
-        listIterator.hasNext();
-        
-        
-        while (listIterator.hasNext()){
-            System.out.println(listIterator.next());
-        }
 
+
+
+
+
+    }
+}
 
 //        System.out.println(list.get(8));
 //        list.set(8, "20");
