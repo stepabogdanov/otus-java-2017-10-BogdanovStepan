0bedd3808047707fa3e7b2b16763b2e6fc90702e
Stepan Bogdanov <stepanbogdanov@yandex.ru>
2017-12-19 20:38:05 +0500

    carry on coding !!!

--- HW08/src/main/java/newJson/ListParser.java
+++ HW08/src/main/java/newJson/ListParser.java
@@ -6,15 +6,19 @@ import javax.json.JsonObjectBuilder;
 import java.lang.reflect.Field;
 import java.util.AbstractCollection;
 import java.util.AbstractList;
+import java.util.ArrayList;
 
 public class ListParser {
     JsonArrayBuilder jsonArrayBuilder;
     public JsonArrayBuilder parse (Field field, Object fieldsValue) throws IllegalAccessException {
         jsonArrayBuilder = Json.createArrayBuilder();
-        if (field.getType().equals(AbstractList.class.getCanonicalName())) {
-
-            System.out.println("ee");
-            return jsonArrayBuilder;
+        System.out.println();
+        if (field.getType().isAssignableFrom(AbstractList.class)) {
+            for (String string: (ArrayList<String>) fieldsValue) {
+                System.out.println(string);
+                jsonArrayBuilder.add(string);
+            }
+        return jsonArrayBuilder;
         }
         return null;
     }
